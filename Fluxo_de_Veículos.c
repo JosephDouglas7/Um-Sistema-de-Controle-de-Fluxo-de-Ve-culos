0b848b63d09c23b9fa186703521dcd3fd0061767
JosephDouglas7 <142358684+JosephDouglas7@users.noreply.github.com>
2024-03-28 22:46:45 -0300

    Update Fluxo_de_Veículos.c

--- Fluxo_de_Veículos.c
+++ Fluxo_de_Veículos.c
@@ -7,34 +7,25 @@
 char tela_menu_principal(void); 
 void tela_sobre(void);  
 void tela_equipe(void); 
-//char modulo_cadastrar_veiculo(void);
 char tela_cadastrar_veiculo(void); 
-char cadastrar_novo_veiculo(void); 
 char alterar_cadastro(void); 
 char excluir_cadastro_v(void);
 char tela_entrada_saida(void); 
-char cadastrar_novo_e_s(void); 
 void alterar_e_s(void); 
 void excluir_e_s(void);
 char tela_relatorios(void);  
-void cadastro_veiculo_r(void); 
-void entrada_saida_r(void);
+
 
 int main(void){ 
    tela_menu_principal();  
    tela_sobre(); 
    tela_equipe();  
-   //modulo_cadastrar_veiculo();
    tela_cadastrar_veiculo();  
-   cadastrar_novo_veiculo();  
    alterar_cadastro(); 
    excluir_cadastro_v();
    tela_entrada_saida(); 
-   cadastrar_novo_e_s(); 
    alterar_e_s();
    tela_relatorios(); 
-   cadastro_veiculo_r(); 
-   entrada_saida_r();
    return 0;
 }  
 
@@ -48,43 +39,25 @@ char menu_principal(void){
   do{
      op = tela_menu_principal();
     switch(op){ 
-      case '1': 
-        op2= tela_cadastrar_veiculo(); 
-         switch(op2){ 
-            case '1': 	cadastrar_novo_veiculo();   
-                        pause();
-                        break;
-            case '2':   alterar_cadastro(); 
-                        pause();
-                        break;
-            case '3': 	excluir_cadastro_v();  
-                        pause();
-                        break;
-        } 		
-    } while (opcao != '4'); 
-    return 0;
-  }
-      case '2':  
-        tela_entrada_saida(); 
-        pausa();
-        break;  
-      case '3': 
-       tela_relatorios(); 
-       pausa();
-       break; 
-      case '4': 
-       tela_sobre(); 
-       pausa();
-       break; 
-      case '5': 
-       tela_equipe(); 
-       pausa();
-       break;
+      case '1': tela_cadastrar_veiculo(); 
+                pause(); 
+                break;
+      case '2': tela_entrada_saida(); 
+                pausa();
+                break;  
+      case '3': tela_relatorios(); 
+                pausa();
+                break; 
+      case '4': tela_sobre(); 
+                pausa();
+                break; 
+      case '5': tela_equipe(); 
+                pausa();
+                break;
     }
   } while (op != '0');
   return 0; 
 }
-
 
 
 char tela_menu_principal(void){ 
@@ -173,10 +146,10 @@ char tela_menu_principal(void){
      printf("\n");   
      printf("\n########################################################################################\n"); 
      printf("\n#                                                                                      #\n");
-     printf("\n#                    = = = = Cadastrar Veículo = = = =                                 #\n"); 
-     printf("\n# 1. Cadastrar um novo veículo                                                         #\n"); 
-     printf("\n# 2. Alterar/editar cadastro de veículo                                                #\n"); 
-     printf("\n# 3. Excluir cadastro de veículo                                                       #\n"); 
+     printf("\n# Nome do motorista:                                                                   #\n"); 
+     printf("\n# Contato do motorista:                                                                #\n"); 
+     printf("\n# Marca do veículo:                                                                    #\n"); 
+     printf("\n# Placa do veículo:                                                                    #\n"); 
      printf("\n#                                                                                      #\n"); 
      printf("\n########################################################################################\n");  
      printf("\nQual sua opcao?:"); 
@@ -184,22 +157,8 @@ char tela_menu_principal(void){
      getchar(); 
      return op;
   }  
-  char cadastrar_novo_veiculo(void){  
-     system("clear||cls");
-     printf("\n");   
-     printf("\n########################################################################################\n"); 
-     printf("\n#                                                                                      #\n"); 
-     printf("\n# Nome do motorista:                                                                   #\n"); 
-     printf("\n# Contato do motorista:                                                                #\n");  
-     printf("\n# Marca do veículo:                                                                    #\n"); 
-     printf("\n# Placa do veículo:                                                                    #\n"); 
-     printf("\n#                                                                                      #\n"); 
-     printf("\n########################################################################################\n"); 
-     printf("\n"); 
-     getchar();  
-     sleep(1); 
-     return 0;
-  }
+
+
   char alterar_cadastro(void){ 
      system("clear||cls");
      printf("\n");   
@@ -212,7 +171,9 @@ char tela_menu_principal(void){
      getchar();  
      sleep(1); 
      return 0;
-  } 
+  }  
+
+
   char excluir_cadastro_v(void){ 
      system("clear||cls");
      printf("\n");   
@@ -226,6 +187,7 @@ char tela_menu_principal(void){
      return 0;
   }
 
+
   char tela_entrada_saida(void){   
     char op;
     system("clear||cls");
@@ -233,9 +195,9 @@ char tela_menu_principal(void){
     printf("\n########################################################################################\n"); 
     printf("\n#                                                                                      #\n");
     printf("\n#                    = = = = =  Entrada/Saída de veículos = = = = =                    #\n");
-    printf("\n#  1. Novo Cadastro                                                                    #\n"); 
-    printf("\n#  2. Alterar/editar cadastro                                                          #\n"); 
-    printf("\n#  4. Excluir cadastro                                                                 #\n");  
+    printf("\n#  Marca do veículo:                                                                   #\n"); 
+    printf("\n#  Placa do veículo:                                                                   #\n"); 
+    printf("\n#  Cor do veículo:                                                                     #\n");  
     printf("\n#                                                                                      #\n"); 
     printf("\n########################################################################################\n"); 
     printf("\nQual sua opcao?:"); 
@@ -245,20 +207,7 @@ char tela_menu_principal(void){
     return 0;
   }  
 
-  char cadastrar_novo_e_s(void){ 
-     system("clear||cls");
-     printf("\n");    
-     printf("\n########################################################################################\n"); 
-     printf("\n#                                                                                      #\n"); 
-     printf("\n# Marca do veículo:                                                                    #\n"); 
-     printf("\n# Placa do veículo:                                                                    #\n"); 
-     printf("\n# Cor do veículo:                                                                      #\n"); 
-     printf("\n#                                                                                      #\n"); 
-     printf("\n########################################################################################\n");  
-     getchar();  
-     sleep(1); 
-     return 0;
-  }
+
   void alterar_e_s(void){ 
      system("clear||cls");
      printf("\n");  
@@ -272,6 +221,7 @@ char tela_menu_principal(void){
      sleep(1); 
      return;
   }
+
 
   void excluir_e_s(void){ 
      system("clear||cls");
@@ -287,16 +237,19 @@ char tela_menu_principal(void){
      return;
   }
 
-  char tela_relatorios(void){  
+  
+ char tela_relatorios(void){  
     char op;
     system("clear||cls");
     printf("\n"); 
     printf("\n########################################################################################\n"); 
     printf("\n#                                                                                      #\n"); 
     printf("\n#                    = = = = = Relatórios = = = = =                                    #\n"); 
-    printf("\n#    1. Cadastro_de_veículos                                                           #\n"); 
-    printf("\n#    2. Entrada_e_Saída_de_veículos                                                    #\n"); 
-    printf("\n#                                                                                      #\n"); 
+    printf("\n#  Nome do motorista:                                                                  #\n");  
+    printf("\n#  Contato do motorista:                                                               #\n");  
+    printf("\n#  Marca do veículo:                                                                   #\n");  
+    printf("\n#  Placa do veículo:                                                                   #\n");   
+    printf("\n#  Cor do veículo:                                                                     #\n"); 
     printf("\n########################################################################################\n");
     printf("\nQual sua opcao?:"); 
     scanf("%s",&op);  
@@ -304,34 +257,4 @@ char tela_menu_principal(void){
     sleep(1); 
     return 0;
   } 
-  void cadastro_veiculo_r(void){ 
-     system("clear||cls");
-     printf("\n"); 
-     printf("\n########################################################################################\n"); 
-     printf("\n#                                                                                      #\n");  
-     printf("\n#                  = = = = Cadastro_de_veículos = = = =                                #\n"); 
-     printf("\n# Nome do motorista:                                                                   #\n"); 
-     printf("\n# Contato do motorista:                                                                #\n");  
-     printf("\n# Marca do veículo:                                                                    #\n"); 
-     printf("\n# Placa do veículo:                                                                    #\n"); 
-     printf("\n#                                                                                      #\n"); 
-     printf("\n########################################################################################\n"); 
-     getchar(); 
-     sleep(1); 
-     return;
-  } 
- void entrada_saida_r(void){ 
-     system("clear||cls");
-     printf("\n"); 
-     printf("\n########################################################################################\n"); 
-     printf("\n#                                                                                      #\n"); 
-     printf("\n#            = = = = Entrada e saída de veículos = = = =                               #\n"); 
-     printf("\n# Marca do veículo:                                                                    #\n"); 
-     printf("\n# Placa do veículo:                                                                    #\n"); 
-     printf("\n# Cor do veículo:                                                                      #\n");       
-     printf("\n#                                                                                      #\n");  
-     printf("\n########################################################################################\n"); 
-     getchar(); 
-     sleep(1);  
-     return;
- } 
+ 
