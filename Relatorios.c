2309ed640a3005739e830b7e4c3b9caaff0354a6
JosephDouglas7 <142358684+JosephDouglas7@users.noreply.github.com>
2024-04-02 19:42:52 -0300

    Update Relatorios.c

--- Relatorios.c
+++ Relatorios.c
@@ -1,1 +1,18 @@
 
+
+char tela_relatorios(void){  
+    int op;
+    system("clear||cls");
+    printf("\n"); 
+    printf("\n########################################################################################\n"); 
+    printf("\n#                                                                                      #\n"); 
+    printf("\n#                    = = = = = Relatórios = = = = =                                    #\n"); 
+    printf("\n#  Nome do motorista:                                                                  #\n");  
+    printf("\n#  Contato do motorista:                                                               #\n");  
+    printf("\n#  Marca do veículo:                                                                   #\n");  
+    printf("\n#  Placa do veículo:                                                                   #\n");   
+    printf("\n#  Cor do veículo:                                                                     #\n"); 
+    printf("\n########################################################################################\n");
+    getchar(); 
+   return 0;
+}
