e639475a77fa5ecfbee6b34c96eb0a84188c6753
JosephDouglas7 <142358684+JosephDouglas7@users.noreply.github.com>
2024-04-02 13:39:24 -0300

    Update Fluxo_de_Veículos.c

--- Fluxo_de_Veículos.c
+++ Fluxo_de_Veículos.c
@@ -40,9 +40,9 @@ char menu_principal(void){
   do{
      op = tela_menu_principal();
     switch(op){ 
-      case  '1' : tela_cadastrar_veiculo(); 
+      case  '1' : modulo_cadastrar_veiculo(); 
                 break;
-      case  '2' : tela_entrada_saida(); 
+      case  '2' : modulo_entrada_saida(); 
                 break;  
       case  '3' : tela_relatorios(); 
                 break; 
@@ -148,11 +148,11 @@ char tela_menu_principal(void){
      printf("\n# 2.alterar cadastro                                                                   #\n"); 
      printf("\n# 3.excluir cadastro                                                                   #\n"); 
      printf("\n#                                                                                      #\n"); 
-     printf("\n########################################################################################\n");  
+     printf("\n########################################################################################\n"); 
      printf("\nQual sua opcao?:"); 
      scanf("%c",&op);  
      getchar();  
-     return 0;
+     return op;
 }  
 
 
@@ -162,41 +162,41 @@ char modulo_cadastrar_veiculo(void){
      op = tela_cadastrar_veiculo();
     switch(op){ 
       case  '1' : cadastrar_veiculo(); 
-                break;
+                  break;
       case  '2' : alterar_cadastro(); 
-                break;  
+                  break;  
       case  '3' : excluir_cadastro_v(); 
-                break; 
+                  break; 
     }
   } while (op != '3'); 
-  return 0; 
+  return 0;
 }  
 
 
  char cadastrar_veiculo(void){  
-     system("clear||cls");
-     printf("\n");   
-     printf("\n########################################################################################\n"); 
-     printf("\n#                                                                                      #\n");
-     printf("\n# Nome do motorista:                                                                   #\n"); 
-     printf("\n# Contato do motorista:                                                                #\n"); 
-     printf("\n# Marca do veículo:                                                                    #\n"); 
-     printf("\n# Placa do veículo:                                                                    #\n"); 
-     printf("\n#                                                                                      #\n"); 
-     printf("\n########################################################################################\n");   
-     getchar(); 
-     return 0;
+    system("clear||cls");
+    printf("\n");
+    printf("\n########################################################################################\n"); 
+    printf("\n#                                                                                      #\n");
+    printf("\n# Nome do motorista:                                                                   #\n"); 
+    printf("\n# Contato do motorista:                                                                #\n"); 
+    printf("\n# Marca do veículo:                                                                    #\n"); 
+    printf("\n# Placa do veículo:                                                                    #\n"); 
+    printf("\n#                                                                                      #\n"); 
+    printf("\n########################################################################################\n"); 
+    getchar(); getchar(); getchar(); 
+    return 0;
 }
 
 
   char alterar_cadastro(void){  
      system("clear||cls");
      printf("\n");   
-     printf("\n########################################################################################\n");  
-     printf("\n#                                                                                      #\n");  
+     printf("\n########################################################################################\n"); 
+     printf("\n#                                                                                      #\n"); 
      printf("\n# Nome do motorista para alterar:                                                      #\n"); 
      printf("\n#                                                                                      #\n"); 
-     printf("\n########################################################################################\n");  
+     printf("\n########################################################################################\n"); 
      printf("\n");  
      getchar(); 
      return 0;
@@ -206,11 +206,11 @@ char modulo_cadastrar_veiculo(void){
   char excluir_cadastro_v(void){  
      system("clear||cls");
      printf("\n");   
-     printf("\n########################################################################################\n");  
-     printf("\n#                                                                                      #\n");  
+     printf("\n########################################################################################\n"); 
+     printf("\n#                                                                                      #\n"); 
      printf("\n# Nome do motorista para excluir:                                                      #\n"); 
      printf("\n#                                                                                      #\n"); 
-     printf("\n########################################################################################\n");    
+     printf("\n########################################################################################\n"); 
      getchar(); 
      return 0;
 }
@@ -231,10 +231,10 @@ char modulo_cadastrar_veiculo(void){
     printf("\nQual sua opcao?:"); 
     scanf("%c",&op); 
     getchar();  
-    return 0;
+    return op;
 }    
 
-       
+
 char modulo_entrada_saida(void){  
   char op;  
   do{
@@ -251,7 +251,7 @@ char modulo_entrada_saida(void){
   return 0; 
 }  
 
-       
+
 char entrada_saida(void){   
     system("clear||cls");
     printf("\n");    
@@ -267,7 +267,7 @@ char entrada_saida(void){
     return 0;
 }  
 
-       
+
   void alterar_e_s(void){  
      system("clear||cls");
      printf("\n");  
