ae1ec4df87287aa40447f35ff2afe3ba65272aed
JosephDouglas7 <142358684+JosephDouglas7@users.noreply.github.com>
2024-04-02 19:26:22 -0300

    Update Cadastrar_veiculo.c

--- Cadastrar_veiculo.c
+++ Cadastrar_veiculo.c
@@ -1,4 +1,11 @@
-   char tela_cadastrar_veiculo(void){  
+char modulo_cadastrar_veiculo(void);
+char tela_cadastrar_veiculo(void);  
+char cadastrar_veiculo(void);
+char alterar_cadastro(void); 
+char excluir_cadastro_v(void);
+
+
+char tela_cadastrar_veiculo(void){  
      char op; 
      system("clear||cls");
      printf("\n");   
