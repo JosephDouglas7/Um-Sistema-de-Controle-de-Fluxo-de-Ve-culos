40dcb86cd543dfc8ee761e78ca34c078bdf0c444
JosephDouglas7 <142358684+JosephDouglas7@users.noreply.github.com>
2024-04-11 11:44:27 -0300

    Update Entrada_saida_V.h

--- Entrada_saida_V.h
+++ Entrada_saida_V.h
@@ -1,5 +1,6 @@
 char modulo_entrada_saida(void);
 char tela_entrada_saida(void); 
-char entrada_saida(void);
+char entrada_saida(void); 
+char pesquisar_e_s(void);
 void alterar_e_s(void); 
 void excluir_e_s(void);
