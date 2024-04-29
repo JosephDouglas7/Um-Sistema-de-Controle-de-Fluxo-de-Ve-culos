549aa18bdfd07f60c78ef7bb347ca47a61704d69
JosephDouglas7 <142358684+JosephDouglas7@users.noreply.github.com>
2024-04-29 11:14:04 -0300

    Update Relatorios.c

--- Relatorios.c
+++ Relatorios.c
@@ -9,11 +9,11 @@ char tela_relatorios(void){
     printf("\n########################################################################################\n"); 
     printf("\n#                                                                                      #\n"); 
     printf("\n#                    = = = = = Relatórios = = = = =                                    #\n"); 
-    printf("\n#  Nome do motorista:                                                                  #\n");  
-    printf("\n#  Contato do motorista:                                                               #\n");  
-    printf("\n#  Marca do veículo:                                                                   #\n");  
-    printf("\n#  Placa do veículo:                                                                   #\n");   
-    printf("\n#  Cor do veículo:                                                                     #\n"); 
+  //printf("\nNome do motorista:",al[i].nome);  
+  //printf("\nContato do motorista:",al[i].celular); 
+  //printf("\nMarca do veículo:",al[i].marca_V); 
+  //printf("\nPlaca do veículo:",al[i].placa_V););  
+  //printf("\nCor do veículo:%[A-ZÁÉÍÓÚÂÊÔÇÀÃÕ a-záéíóúâêôçàãõ]",novo_e_s_v->cor_V); 
     printf("\n########################################################################################\n");
     getchar(); 
    return 0;
