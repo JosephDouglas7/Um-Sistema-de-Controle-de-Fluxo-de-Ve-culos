7f64c78ab82f71b2c61e2d40a774ff246fb7152f
JosephDouglas7 <142358684+JosephDouglas7@users.noreply.github.com>
2024-04-30 14:43:44 -0300

    Update cadastrar_veiculo.h

--- cadastrar_veiculo.h
+++ cadastrar_veiculo.h
@@ -1,6 +1,6 @@
 char modulo_cadastrar_veiculo(void);
 char tela_cadastrar_veiculo(void);  
-char cadastrar_veiculo(char); 
-char pesquisar_cadastro_v(char);
+void cadastrar_veiculo(void); 
+void pesquisar_cadastro_v(void);
 char alterar_cadastro(void); 
 char excluir_cadastro_v(void);  
