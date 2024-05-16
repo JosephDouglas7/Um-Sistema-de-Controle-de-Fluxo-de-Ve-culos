bf6e70500851f1efb8abcede3a55c9c053a980a4
JosephDouglas7 <142358684+JosephDouglas7@users.noreply.github.com>
2024-05-16 19:27:03 -0300

    Update entrada_saida_v.c

--- entrada_saida_v.c
+++ entrada_saida_v.c
@@ -1,29 +1,28 @@
-#include<stdio.h>   
+#include<stdio.h>  
 #include<stdlib.h> 
-#include"entrada_saida_v.h" 
-
+#include"entrada_saida_V.h"
 
 typedef struct entrada_s entrada_S;
-typedef struct entrada_s{
+
+struct entrada_s{
   char marca_V[20]; 
   char placa_V[9]; 
   char cor_V[20]; 
 }; 
 
-
 char tela_entrada_saida(void){   
     char op; 
     system("clear||cls");
     printf("\n");    
-    printf("\n########################################################################################\n"); 
-    printf("\n#                                                                                      #\n");
-    printf("\n#                    = = = = =  Entrada/Saída de veículos = = = = =                    #\n");
-    printf("\n#  1.Cadastrar e.s                                                                     #\n");  
-    printf("\n#  2.Pesquisar e.s                                                                     #\n");
-    printf("\n#  3.Alterar e.s                                                                       #\n"); 
-    printf("\n#  4.Excluir e.s                                                                       #\n");  
-    printf("\n#  5.retornar ao menu principal                                                        #\n"); 
-    printf("\n########################################################################################\n");  
+    printf("\n#######################################################\n"); 
+    printf("\n#                                                     #\n");
+    printf("\n#          = =  Entrada/Saída de veículos = =         #\n");
+    printf("\n#  1.Cadastrar e.s                                    #\n"); 
+    printf("\n#  2.Pesquisar e.s                                    #\n");  
+    printf("\n#  3.Alterar e.s                                      #\n"); 
+    printf("\n#  4.Excluir e.s                                      #\n"); 
+    printf("\n#  5. Voltar ao menu principal                        #\n");      
+    printf("\n#######################################################\n"); 
     printf("\nQual sua opcao?:"); 
     scanf("%c",&op); 
     getchar();  
@@ -44,20 +43,22 @@ char modulo_entrada_saida(void){
                   break;  
       case  '4' : excluir_e_s(); 
                   break;  
-      case  '5' : tela_menu_principal(); 
-                  break;
+      case  '5': tela_menu_principal(); 
+                 break;
     }
   } while (op != '5'); 
   return 0; 
 }  
 
 
-char entrada_saida(void){    
+void entrada_saida(void){  
     entrada_S cv;
+  
     system("clear||cls");
     printf("\n");    
-    printf("\n########################################################################################\n"); 
-    printf("\n#                    = = = = =  Entrada/Saída de veículos = = = = =                    #\n");
+    printf("\n#############################################\n"); 
+    printf("\n#                                           #\n");
+    printf("\n#       =  Entrada/Saída de veículos =      #\n");
     printf("\nMarca do veículo:");  
     scanf(" %20[^\n]", cv.marca_V);
     printf("\nPlaca do veículo:");  
@@ -65,30 +66,31 @@ char entrada_saida(void){
     printf("\nCor do veículo:");   
     scanf(" %20[^\n]", cv.cor_V);
     getchar();   
-    sleep(1);    
+    sleep(1);     
 }  
 
 
-char pesquisar_e_s(void){ 
-     system("clear||cls");
-     printf("\n");  
-     printf("\n########################################################################################\n");  
-     printf("\n#                                                                                      #\n");  
-     printf("\nPlaca do veículo:");  
-     scanf(" %8[^\n]", cv.placa_V);
-     printf("\n");  
-     getchar();  
+char pesquisar_e_s(void){  
+    entrada_S cv; 
+  
+    system("clear||cls");
+    printf("\n");     
+    printf("\n############################################\n");
+    printf("\n#                                          #\n"); 
+    printf("\n#  - - - Pesquisar entrada/saida.v - - -   #\n");  
+    printf("\nPlaca do veículo:");  
+    scanf(" %8[^\n]", cv.placa_V);
+    printf("\n");  
+    getchar();   
+    return 0;
 }
 
   void alterar_e_s(void){  
      system("clear||cls");
-     printf("\n");  
-     printf("\n########################################################################################\n");  
-     printf("\n#                                                                                      #\n");  
-     printf("\n# Placa do veículo para alterar:                                                       #\n"); 
-     printf("\n#                                                                                      #\n"); 
-     printf("\n########################################################################################\n");  
-     printf("\n");  
+     printf("\n");      
+     printf("\n##########################################\n");  
+     printf("\n#                                        #\n");  
+     printf("\nPlaca do veículo para alterar:");   
      getchar();  
      return;
 }
@@ -96,11 +98,10 @@ char pesquisar_e_s(void){
 
   void excluir_e_s(void){  
      system("clear||cls");
-     printf("\n");  
-     printf("\n########################################################################################\n");   
-     printf("\n#                                                                                      #\n");
-     printf("\nPlaca do veículo para excluir:");  
-     printf("\n");   
+     printf("\n");       
+     printf("\n########################################\n");  
+     printf("\n#                                      #\n"); 
+     printf("\nPlaca do veículo para excluir:");   
      getchar();  
      return;
 }
