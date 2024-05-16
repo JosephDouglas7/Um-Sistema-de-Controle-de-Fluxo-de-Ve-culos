6e36e0121521a8d33b108bb555c6ed2986258bc6
JosephDouglas7 <142358684+JosephDouglas7@users.noreply.github.com>
2024-05-16 19:27:53 -0300

    Update relatorios.c

--- relatorios.c
+++ relatorios.c
@@ -1,4 +1,4 @@
-#include<stdio.h>  
+#include<stdio.h>   
 #include<stdlib.h> 
 #include"relatorios.h" 
 
@@ -6,15 +6,10 @@
 char tela_relatorios(void){  
     system("clear||cls");
     printf("\n"); 
-    printf("\n########################################################################################\n"); 
-    printf("\n#                                                                                      #\n"); 
-    printf("\n#                    = = = = = Relatórios = = = = =                                    #\n"); 
-  //printf("\nNome do motorista:",al[i].nome);  
-  //printf("\nContato do motorista:",al[i].celular); 
-  //printf("\nMarca do veículo:",al[i].marca_V); 
-  //printf("\nPlaca do veículo:",al[i].placa_V););  
-  //printf("\nCor do veículo:%[A-ZÁÉÍÓÚÂÊÔÇÀÃÕ a-záéíóúâêôçàãõ]",novo_e_s_v->cor_V); 
-    printf("\n########################################################################################\n");
+    printf("\n#################################################\n"); 
+    printf("\n#                                               #\n"); 
+    printf("\n#            = = Relatórios = =                 #\n"); 
+    printf("\n#################################################\n");
     getchar(); 
    return 0;
 }
