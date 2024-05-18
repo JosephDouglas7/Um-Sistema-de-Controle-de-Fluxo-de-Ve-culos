8dba96fa7c14608defefe532d94b96f3f7855dc7
JosephDouglas7 <142358684+JosephDouglas7@users.noreply.github.com>
2024-05-18 10:51:20 -0300

    Update relatorios.c

--- relatorios.c
+++ relatorios.c
@@ -1,14 +1,42 @@
 #include<stdio.h>   
 #include<stdlib.h> 
-#include"relatorios.h" 
+#include"relatorios.h"  
+#include "cliente.h" 
+#include"validar.h"
+#include"veiculo.h"
 
 
 char tela_relatorios(void){  
+    Cliente *vco1;
+    vco1 = (Cliente*) malloc(sizeof(Cliente));
     system("clear||cls");
     printf("\n"); 
-    printf("\n#################################################\n"); 
-    printf("\n#                                               #\n"); 
-    printf("\n#            = = Relatórios = =                 #\n"); 
+    printf("\n#####################################\n"); 
+    printf("\n#                                   #\n"); 
+    printf("\n#         = = Relatórios = =        #\n");  
+    printf("\nNome do motorista:");  
+    scanf(" %19[^\n]",vco1->nome); 
+    validar_nome(vco1->nome);
+    printf("\nContato do motorista:");  
+    scanf(" %8[^\n]",vco1->celular); 
+    validar_nome(vco1->celular); 
+    printf("\nCPF:"); 
+    scanf(" %11[^\n]",vco1->cpf);  
+    validar_nome(vco1->cpf);  
+    printf("\nMarca do veículo:");  
+  //scanf(" %19[^\n]",vco1->marca_V); 
+  //validar_nome(vco1->marca_V);
+  //printf("\nPlaca do veículo:");  
+  //scanf(" %8[^\n]",vco1->placa_V); 
+  //validar_nome(vco1->placa_V); 
+  //printf("\nMês:"); 
+  //scanf(" %2[^\n]",vco1->mes_V); 
+  //validar_nome(vco1->mes_V); 
+  //printf("\nAno:"); 
+  //scanf(" %4[^\n]",vco1->ano_V); 
+  //validar_nome(vco1->ano_V);
+  //getchar(); 
+    sleep(1);
     printf("\n#################################################\n");
     getchar(); 
    return 0;
