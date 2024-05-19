c2d354784c8ba8e767963f47dc799410bbacdc7c
JosephDouglas7 <142358684+JosephDouglas7@users.noreply.github.com>
2024-05-18 21:49:34 -0300

    Update cliente.h

--- cliente.h
+++ cliente.h
@@ -8,8 +8,19 @@ struct cliente{
 };   
 
 
-void cadastrar_cliente(void);
+void cadastrar_cliente(void); 
+char* tela_pesquisar_cliente(void); 
+char* tela_pesquisar_cliente(void);
+Cliente* buscar_cliente(char* placa);
+Cliente* preencher_cliente(void);
 char tela_cadastrar_cliente(void); 
 void pesquisar_cliente(void); 
-void alterar_cliente(void); 
-void excluir_cliente(void);
+void alterar_cliente(void);  
+char* tela_alterar_cliente(void);
+char* tela_alterar_cliente(void);
+void excluir_cliente(void);  
+char* tela_excluir_cliente(void);
+void gravar_cliente(Cliente* vco1); 
+void erro_arquivo_cliente(void);  
+void exibir_cliente(Cliente* carro); 
+void regravar_cliente(Cliente* vco1);
