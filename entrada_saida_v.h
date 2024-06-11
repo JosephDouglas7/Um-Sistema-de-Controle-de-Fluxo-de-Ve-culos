4f7fcd0285a1be356dfa87fdb6cfb3d5acd4b284
JosephDouglas7 <142358684+JosephDouglas7@users.noreply.github.com>
2024-06-11 19:29:22 -0300

    Update entrada_saida_v.h

--- entrada_saida_v.h
+++ entrada_saida_v.h
@@ -1,6 +1,26 @@
+typedef struct fluxo_veiculo Fluxo_Veiculo;
+
+struct fluxo_veiculo{
+  char marca_V[21]; 
+  char placa_V[9]; 
+  char cor_V[12];  
+  int status;
+};   
+
+
+
 char modulo_entrada_saida(void);
 char tela_entrada_saida(void); 
-char entrada_saida(void); 
-char pesquisar_e_s(void);
-void alterar_e_s(void); 
-void excluir_e_s(void);
+void entrada_saida(void);  
+Fluxo_Veiculo* preencher_entrada_saida(void);
+void pesquisar_entrada_saida(void);  
+void exibir_entrada_saida(Fluxo_Veiculo* carro); 
+Fluxo_Veiculo* buscar_entrada_saida(char* placa);
+char* tela_pesquisar_entrada_saida(void);
+void alterar_entrada_saida(void);  
+char* tela_alterar_entrada_saida(void);
+void excluir_entrada_saida(void);  
+char* tela_excluir_entrada_saida(void); 
+void gravar_entrada_saida(Fluxo_Veiculo* vco1); 
+void erro_arquivo_entrada_saida(void);
+
