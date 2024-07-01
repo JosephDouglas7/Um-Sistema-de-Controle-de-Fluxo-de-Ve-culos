0ff37992de48cc0dd83737d0053c267efe69ce40
JosephDouglas7 <142358684+JosephDouglas7@users.noreply.github.com>
2024-07-01 10:12:28 -0300

    Update validar.h

--- validar.h
+++ validar.h
@@ -1,7 +1,7 @@
 int validar_celular(char *celular);  
-int valida_cpf(char* cpf);  
+int validar_cpf(char* cpf);  
 int validar_nome(char *nome);  
-int valida_placa(char *placa);  
+int validar_placa(char *placa);  
 float validar_aluguel(float *aluguel_V); 
 float validar_entrada_V(float *entrada_V); 
 float validar_saida_V(float *saida_V);
