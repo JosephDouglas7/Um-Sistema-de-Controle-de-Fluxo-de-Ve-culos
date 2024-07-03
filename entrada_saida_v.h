92d11e4851307ee122a3c1008c1a45d015592890
JosephDouglas7 <142358684+JosephDouglas7@users.noreply.github.com>
2024-07-03 07:20:20 -0300

    Update entrada_saida_v.h

--- entrada_saida_v.h
+++ entrada_saida_v.h
@@ -1,16 +1,13 @@
 typedef struct fluxo_veiculo Fluxo_Veiculo;
 
 struct fluxo_veiculo{
-  char nome[100]; 
-  char celular[13]; 
   char cpf[12];  
   char placa_V[12]; 
-  float entrada_V; 
-  float saida_V; 
+  char entrada_V[7]; 
+  char saida_V[7]; 
   float aluguel_V;
   int status;
 };   
-
 
 
 char modulo_entrada_saida(void);
@@ -28,10 +25,8 @@ char* tela_excluir_entrada_saida(void);
 void gravar_entrada_saida(Fluxo_Veiculo*); 
 void erro_arquivo_entrada_saida(void);
 void regravar_entrada_saida(Fluxo_Veiculo*); 
-void ler_nome1(char*); 
-void ler_celular1(char*); 
 void ler_cpf1(char*);
 void ler_placa1(char*); 
-void ler_entrada(float*);
-void ler_saida(float*); 
+void ler_entrada(char*);
+void ler_saida(char*); 
 void ler_aluguel_V(float*); 
