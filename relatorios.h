6e8d93527dd65cb196a5f1d9f8616151a9a0d714
JosephDouglas7 <142358684+JosephDouglas7@users.noreply.github.com>
2024-07-24 10:21:35 -0300

    Update relatorios.h

--- relatorios.h
+++ relatorios.h
@@ -1,1 +1,8 @@
-char tela_relatorios(void); 
+void modulo_relatorio(void); 
+char menu_relatorio(void);
+void lista_cliente(void);    
+void relatorios_clientes(void); 
+void relatorios_veiculos(void); 
+void lista_veiculo(void); 
+void relatorios_fluxo_veiculos(void); 
+void lista_fluxo_veiculos(void);
