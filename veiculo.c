301b3384a1e4b1f2ec121f14819c63394e4f8383
JosephDouglas7 <142358684+JosephDouglas7@users.noreply.github.com>
2024-07-24 10:19:33 -0300

    Update veiculo.c

--- veiculo.c
+++ veiculo.c
@@ -21,12 +21,12 @@ void cadastrar_veiculo(void) {
 
 
 void pesquisar_veiculo(void) {
-  Veiculo *carro;
-  char *placa;
+  Veiculo* veiculo;
+  char* placa;
   placa = tela_pesquisar_veiculo();
-  carro = buscar_veiculo(placa);
-  exibir_veiculo(carro);
-  free(carro);
+  veiculo = buscar_veiculo(placa);
+  exibir_veiculo(veiculo);
+  free(veiculo);
   free(placa);
 }
 
@@ -69,14 +69,14 @@ char tela_cadastrar_veiculo(void) {
   char op;
   system("clear||cls");
   printf("\n");
-  printf("\n############################################\n");
-  printf("\n#     = =  tela cadastrar veiculo = =      #\n");
-  printf("\n# 1.cadastrar veiculo                      #\n");
-  printf("\n# 2.Pesquisar veiculo                      #\n");
-  printf("\n# 3.alterar cadastro                       #\n");
-  printf("\n# 4.excluir cadastro                       #\n");
-  printf("\n# 5.Retornar ao menu principal             #\n");
-  printf("\n############################################\n");
+  printf("\n#############################################\n");
+  printf("\n#    = = =  tela cadastrar veiculo = = =    #\n");
+  printf("\n# 1.cadastrar veiculo                       #\n");
+  printf("\n# 2.Pesquisar veiculo                       #\n");
+  printf("\n# 3.alterar cadastro                        #\n");
+  printf("\n# 4.excluir cadastro                        #\n");
+  printf("\n# 5.Retornar ao menu principal              #\n");
+  printf("\n#############################################\n");
   printf("\nQual sua opcao?:");
   scanf("%c", &op);
   getchar();
@@ -107,29 +107,28 @@ void modulo_cadastrar_veiculo(void) {
 
 
 Veiculo *preencher_veiculo(void) {
-  Veiculo *vco1;
-  vco1 = (Veiculo *)malloc(sizeof(Veiculo));
+  Veiculo *veiculo;
+  veiculo = (Veiculo *)malloc(sizeof(Veiculo));
   system("clear||cls");
   printf("\n");
   printf("\nMarca do veiculo:"); 
-  scanf("%s", vco1->marca_V);
-  ler_placa(vco1->placa_V); 
-  ler_ano(vco1-> ano_V);
-  vco1->status = 1;
+  scanf("%s", veiculo->marca_V);
+  ler_placa(veiculo->placa_V); 
+  ler_ano(veiculo->ano_V);
+  veiculo->status = 1;
   getchar();
   sleep(1);
-  return vco1;
+  return veiculo;
 }
 
 
-void ler_placa(char* placa_V) {
+void ler_placa1(char* placa) {
     printf("Digite a placa do veículo:");
-    scanf("%s", placa_V);
-    while (!validar_placa(placa_V)) {
+    scanf("%s", placa);
+    while (!validar_placa(placa)) {
       printf("Placa inválida!\n");
       printf("Digite a placa do veículo:");
-      scanf("%s", placa_V); 
-      break;
+      scanf("%s", placa); 
     }
 }    
 
@@ -141,20 +140,19 @@ void ler_ano(char* ano_V) {
       printf("Ano inválida!\n");
       printf("Digite o ano do veículo:");
       scanf("%s", ano_V); 
-      break;
     }
 } 
 
 
 char *tela_pesquisar_veiculo(void) {
   char *vco;
-  vco = (char *)malloc(9 * sizeof(char));
+  vco = (char *)malloc(12*sizeof(char));
   system("clear||cls");
   printf("\n"); 
   printf("\n- - - Pesquisar veiculo - - -\n");
   printf("\nPlaca do veículo:");
-  scanf(" %8[^\n]", vco);
-  fgets(vco, sizeof(vco), stdin);
+  scanf(" %8[^\n]", vco);  
+  getchar();
   return vco;
 }
 
@@ -221,16 +219,15 @@ Veiculo *buscar_veiculo(char *placa) {
 }
 
 
-void exibir_veiculo(Veiculo *vco1) {
-  vco1 = (Veiculo *)malloc(sizeof(Veiculo));
-  if (vco1 == NULL) {
+void exibir_veiculo(Veiculo *veiculo) { 
+  if (veiculo == NULL) { 
     printf("\n= = = Veiculo Inexistente = = =\n");
   } else {
     printf("\n= = = Veiculo Cadastrado = = =\n");
-    printf("Marca: %s\n", vco1->marca_V);
-    printf("Placa: %s\n", vco1->placa_V);
-    printf("Ano: %s\n", vco1->ano_V);
-    printf("Status: %d\n", vco1->status);
+    printf("Marca: %s\n", veiculo->marca_V);
+    printf("Placa: %s\n", veiculo->placa_V);
+    printf("Ano: %s\n",veiculo->ano_V);
+    printf("Status: %d\n", veiculo->status);
   }
   printf("\n\nTecle ENTER para continuar!\n\n");
   getchar();
